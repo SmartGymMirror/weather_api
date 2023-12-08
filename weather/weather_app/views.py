14d5269e61976cd69943a2e27e949d919f3e8d1e
albabernal03 <ALBABERO@MYUAX.COM>
2023-12-08 19:48:13 +0100

    pasamos ahora con la url

--- weather/weather_app/views.py
+++ weather/weather_app/views.py
@@ -12,3 +12,20 @@ def get_weather(request):
     
     api_key ='' # Aquí va tu API KEY de OpenWeatherMap
     weather_api_url = f'https://api.openweathermap.org/data/2.5/weather?q={localizacion}&appid={api_key}&units=metric'
+
+    try:
+        response=requests.get(weather_api_url)
+        response.raise_for_status()# Si hay un error en la petición, se lanza una excepción
+        weather_data = {
+            'humedad': response.json()['main']['humidity'],
+            'temperatura': response.json()['main']['temp'],
+            'descripcion': response.json()['weather'][0]['description'],
+            'icono': response.json()['weather'][0]['icon'],
+
+        }
+        return JsonResponse(weather_data, status=200)
+    except requests.exceptions.HTTPError as errh:
+        return JsonResponse({'error': f'Error HTTP: {errh}'}) 
+    except requests.exceptions.RequestException as err:
+        return JsonResponse({'error': f'Error de solicitud: {err}'}, status=500)
+
