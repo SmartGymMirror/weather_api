b9fc9df09e25d872f81fc8c535b1441f31dfabac
albabernal03 <ALBABERO@MYUAX.COM>
2023-12-08 19:42:50 +0100

    estamos hacion la vista que nos coge el tiempo

--- weather/weather_app/views.py
+++ weather/weather_app/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import JsonResponse
 import requests
+
+def get_weather(request):
+    localizacion = request.GET.get('localizacion')
+
+    if not localizacion:
+        return JsonResponse({'error': 'Debes especificar una localización'}, status=400)
+    
+    api_key ='' # Aquí va tu API KEY de OpenWeatherMap
+    weather_api_url = f'https://api.openweathermap.org/data/2.5/weather?q={localizacion}&appid={api_key}&units=metric'
